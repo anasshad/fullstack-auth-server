9302d39428ca41f2227d0bda7bf8ab75338edeca
Anas Shad <anasshad@hotmail.com>
2018-10-24 20:31:02 +0500

    set callback url

--- auth/config.js
+++ auth/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'facebookAuth': {
         clientID: '315032279104615',
         clientSecret: 'b12dcf6d01b0859a0b45f9b3f4769bad',
-        callbackURL: "http://localhost:8000/auth/facebook/callback",
+        callbackURL: "https://rocky-castle-22748.herokuapp.com/auth/facebook/callback",
         profileFields: ['id', 'emails', 'name', 'picture']
     }
 }
